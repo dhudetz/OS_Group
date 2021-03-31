2a5d055af6f504bf34c8c39f94fcc2495c5d466d
Martin Boehm <mboehm1@morbius.mscsnet.mu.edu>
2021-03-30 21:27:09 -0500

    added several testcases

--- xinu-hw7/system/testcases.c
+++ xinu-hw7/system/testcases.c
@@ -31,7 +31,12 @@ void testcases(void)
     uchar character;
 
     kprintf("===TEST BEGIN===\r\n");
-    kprintf("0) Test priority scheduling\r\n");
+    kprintf("0: Basic getmem and freemem test\r\n");
+    kprintf("1: Call getmem, free it, call it again, and free it\r\n");
+    kprintf("2: Call getmem, free it, call for a mem larger than the splintered portion, and free\r\n");
+    kprintf("3: Call getmem twice, free it twice\r\n");
+    kprintf("4: Call getmem of 0\r\n");
+    kprintf("5: Check to see what happens when getmem is called with a requested 1 bit\r\n");
 
     // TODO: Test your operating system!
     register memblk *a, *b, *c;
@@ -65,6 +70,30 @@ void testcases(void)
 	b = getmem(32);
 	print_freelist(0);
 	freemem(b, 32);
+	print_freelist(0);
+	break;
+    case '3':
+	print_freelist(0);
+	a = getmem(64);
+	print_freelist(0);
+	b = getmem(32);
+	print_freelist(0);
+	freemem(a, 64);
+	print_freelist(0);
+	freemem(b, 32);
+	print_freelist(0);
+	break;
+    case '4':
+	if(getmem(0) == SYSERR)
+	{
+	    kprintf("SYSERR HAS OCCURED (THIS IS A GOOD THING)\r\n");
+	}
+	break;
+    case '5':
+	print_freelist(0);
+	a = getmem(1);
+	print_freelist(0);
+	freemem(a, 1);
 	print_freelist(0);
 	break;
     default:
