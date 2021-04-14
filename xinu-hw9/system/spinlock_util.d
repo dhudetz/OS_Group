28beec5ff12aa0dd86af2fe81463108be48910d9
dhudetz <dannyhudetz@gmail.com>
2021-04-14 10:43:54 -0500

    finished 8 hw9 testcases

--- xinu-hw9/system/spinlock_util.d
+++
@@ -1,4 +0,0 @@
-../system/spinlock_util.o: ../system/spinlock_util.S \
- ../include/spinlock.h
-
-../include/spinlock.h:
--- xinu-hw9/system/testcases.c
+++ xinu-hw9/system/testcases.c
@@ -35,14 +35,15 @@ void testcases(void)
     semaphore testsem;
 
     enable();
-
+    char word[] = "hello";
+    int i = 0;
     printf("===TEST BEGIN===\r\n");
-    
+    printf("Tests 1->8\r\n");
 	// TODO: Test your operating system!
     c = getc();
     switch (c)
     {
-    case '0':
+    case '1':
 	printf("Test 1: Type char 'a': \r\n");
 	c = getc();
 	switch (c)
@@ -54,6 +55,71 @@ void testcases(void)
 		printf("FAILED: Character '%c' detected instead of 'a'.\r\n", c);
 		break;
 	}
+	break;
+    case '2':
+	printf("Test 2: Type \"hello\": \r\n");
+	int flag = 1;
+	for(i = 0; i<5; i++)
+	{
+		int c = getc();
+		if(word[i] != c)
+			flag = 0;
+	}
+	switch(flag)
+	{
+	case(0):
+		printf("FAILED: String other than 'hello' detected.\r\n");
+		break;
+	case(1):
+		printf("PASSED: String detected correctly.\r\n");
+		break;
+	}
+	break;
+    case '3':
+	printf("Test 3: putc() test.\r\n");
+	putc('*');
+	printf("\r\nIf you see a '*' char above, then test PASSED. Else, FAILED.\r\n");
+	break;
+    case '4':
+	printf("Test 4: Multiple putc() test. \r\n");
+	for(i = 0; i < 5; i++)
+	{
+		putc(word[i]);
+	}
+	printf("\r\nIf you see 'hello' above, then test PASSED. Else, FAILED.\r\n");
+	break;
+    case '5':
+	printf("Test 5: Semaphore wait test. \r\n");
+	testsem = semcreate(1);
+	print_sem(testsem);
+	wait(testsem);
+	print_sem(testsem);
+	break;
+    case '6':
+	printf("Test 5: Semaphore signal test. \r\n");
+	testsem = semcreate(1);
+	print_sem(testsem);
+	signal(testsem);
+	print_sem(testsem);
+	break;
+    case '7':
+	printf("Test 7: Semaphore wait signal wait test. \r\n");
+	testsem = semcreate(1);
+	print_sem(testsem);
+	wait(testsem);
+	print_sem(testsem);
+	signal(testsem);
+	print_sem(testsem);
+	wait(testsem);
+	print_sem(testsem);
+
+	break;
+    case '8':
+	printf("Test 8: Semaphore signaln test. \r\n");
+	testsem = semcreate(1);
+	print_sem(testsem);
+	signaln(testsem, 3);
+	print_sem(testsem);
 	break;
     default:
         break;
